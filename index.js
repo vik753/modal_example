c420f13349929bca47497f40db8a99ee95e78269
Igor Korenets <vik753@gmail.com>
2020-02-19 15:29:39 +0200

    Modal title and body are able to set. Buttons and close X are working and return modal result.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const modal = $.modal();
+const modal = $.modal({
+  title: "Do you really want to buy this tickets?",
+  bodyText: "Sunday at am10.30 Movie: 'Kill them all.' Two tickets. Sit12, Row 10. Sit 13, row 10.",
+});
+
+// UI elements
+const closeBtn = document.querySelector(".modal-close");
+const modalFooter = document.querySelector('.modal-footer');
+
+// Program
+closeBtn.addEventListener("click", e => {
+	const answer = modal.close();
+	console.log(answer);
+});
+
+// Btn events
+modalFooter.addEventListener('click', e => {
+	if (e.target.id === 'okBtn') {
+		modal.result = true;
+	};
+	if (e.target.id === 'cancelBtn') {
+		modal.result = false;
+	};
+	console.log(modal);
+	modal.close();
+	return modal.result;
+});
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,26 +1,30 @@
 $.modal = function(options) {
-	const $modal = _createModal(options);
-	let _isClosing = false;
-	const ANIMATION_SPEED = 200;
+  const $modal = _createModal(options);
+  let _isClosing = false;
+  const ANIMATION_SPEED = 200;
 
   return {
+    result: false,
     open() {
       !_isClosing && $modal.classList.add("open");
     },
-		close() {
-			_isClosing = true;
+    close() {
+      _isClosing = true;
       $modal.classList.remove("open");
       $modal.classList.add("hide");
       setTimeout(() => {
-				$modal.classList.remove("hide");
-				_isClosing = false;
-			}, ANIMATION_SPEED);
+        $modal.classList.remove("hide");
+        _isClosing = false;
+      }, ANIMATION_SPEED);
+      this.result = false;
+      return this.result;
     },
     destroy() {},
   };
 };
 
 function _createModal(options) {
+  const { title = "Modal Title", bodyText = "Test body text." } = options;
   const modal = document.createElement("div");
   modal.classList.add("vmodal");
   modal.insertAdjacentHTML(
@@ -29,16 +33,15 @@ function _createModal(options) {
 		<div class="modal-shadow">
 			<div class="modal-window">
 				<div class="modal-header">
-					<span class="modal-title">Modal Title</span>
+					<span class="modal-title">${title}</span>
 					<span class="modal-close">&times;</span>
 				</div>
 				<div class="modal-body">
-					<p>Lorem ipsum dolor sit amet.</p>
-					<p>Lorem ipsum dolor sit amet.</p>
+					<p>${bodyText}</p>
 				</div>
 				<div class="modal-footer">
-					<button class="btn btn-primary">Ok</button>
-					<button class="btn btn-danger">Cancel</button>
+					<button id="okBtn" class="btn btn-primary">Ok</button>
+					<button id="cancelBtn" class="btn btn-danger">Cancel</button>
 				</div>
 			</div>
 		</div>
